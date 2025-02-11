8acb81eb50f27d269562b9bd2a36de6b640652ca
bluehotdog-autonomy <195765193+bluehotdog-autonomy@users.noreply.github.com>
2025-02-11 11:47:39 +0200

    adding various more packages

---
+++ dot_config/private_fish/conf.d/aliases.fish
@@ -0,0 +1,10 @@
+if status --is-interactive
+    alias cat 'bat'
+    alias lg 'lazygit'
+
+    alias l 'ls -l'
+    alias la 'ls -a'
+    alias lla 'ls -la'
+    alias lt 'ls --tree'
+    alias ls 'lsd'
+end
--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
 end
 
 set -gx EDITOR cursor --wait
+set -gx MANPAGER "sh -c 'sed -u -e \"s/\\x1B\\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
+man 2 select
 
 fish_add_path /opt/homebrew/sbin
 
