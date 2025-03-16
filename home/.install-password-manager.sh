6a3f166d3aefe36a81a4e0d7789c4073907f5aac
Danni Friedland <BlueHotDog@users.noreply.github.com>
2025-03-16 16:37:51 +0100

    Update .install-password-manager.sh

--- home/.install-password-manager.sh
+++ home/.install-password-manager.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 set -ex
 
-if type op >/dev/null 2>&1; then
-    echo "1Password CLI is already installed"
-else
-    case "$(uname -s)" in
-    Darwin)
-        brew install --cask 1password
+
+case "$(uname -s)" in
+Darwin)
+    if [ ! -d "$DIRECTORY" ]; then
+      brew install --cask 1password
+    fi
+    
+    if type op >/dev/null 2>&1; then
+        echo "1Password CLI is already installed"
+    else
         brew install 1password-cli
-        ;;
-    *)
-        echo "unsupported OS"
-        exit 1
-        ;;
-    esac
-fi
+    fi
+;;
+*)
+    echo "unsupported OS"
+    exit 1
+;;
+esac
 
 read -p "Please open 1Password, log into all accounts and set under Settings>CLI activate Integrate with 1Password CLI. Press any key to continue." -n 1 -r
 echo
